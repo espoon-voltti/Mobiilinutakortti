ea39c3f42857704d3508831ee7b8bca899a64d6a
RichardPeltonen <adm-richard.peltonen@espoo.fi>
2024-11-06 10:45:15 +0200

    More password cleanup

--- backend/src/admin/admin.service.ts
+++ backend/src/admin/admin.service.ts
@@ -8,13 +8,7 @@ import { Repository } from 'typeorm';
 import { Admin, Lockout } from './entities';
 import * as content from '../content';
 import { EditAdminDto, RegisterAdminDto } from './dto';
-import { hash, compare } from 'bcrypt';
-import {
-  saltRounds,
-  maximumAttempts,
-} from '../authentication/authentication.consts';
 import { AdminUserViewModel } from './vm/admin.vm';
-import { ChangePasswordDto } from './dto/changePassword.dto';
 
 /**
  * A service designed to deal with Admin actions.
@@ -62,28 +56,6 @@ export class AdminService {
   }
 
   /**
-   * Returns the lockout record of the admin.
-   * @param admin - the id of the admin.
-   */
-  async getLockoutRecord(adminId: string): Promise<Lockout> {
-    const admin = await this.getAdmin(adminId);
-    if (!admin) {
-      throw new BadRequestException(content.UserNotFound);
-    }
-    return await this.lockoutRepo.findOne({
-      where: { admin },
-      relations: ['admin'],
-    });
-  }
-
-  async deleteLockoutRecord(adminId: string) {
-    const lockoutRecord = await this.getLockoutRecord(adminId);
-    if (lockoutRecord) {
-      await this.lockoutRepo.remove(lockoutRecord);
-    }
-  }
-
-  /**
    * @param registrationData - the details to register.
    * @returns Promise<string> - a success message.
    */
@@ -92,38 +64,16 @@ export class AdminService {
     if (userExists) {
       throw new ConflictException(content.AdminAlreadyExists);
     }
-    const hashedPassword = await hash(registrationData.password, saltRounds);
     const admin = {
       firstName: registrationData.firstName,
       lastName: registrationData.lastName,
       email: registrationData.email,
-      password: hashedPassword,
+      password: null,
       isSuperUser: registrationData.isSuperUser,
       mainYouthClub: registrationData.mainYouthClub,
     } as Admin;
     await this.createAdmin(admin);
     return content.Created(registrationData.email);
-  }
-
-  async changePassword(
-    adminId: string,
-    changePasswordDto: ChangePasswordDto,
-  ): Promise<string> {
-    const user = await this.adminRepo.findOneBy({ id: adminId });
-    if (!user) {
-      throw new BadRequestException(content.UserNotFound);
-    }
-    const passwordsMatch = await compare(
-      changePasswordDto.oldPassword,
-      user.password,
-    );
-    if (!passwordsMatch) {
-      throw new BadRequestException(content.IncorrectPassword);
-    }
-    const newPassword = await hash(changePasswordDto.newPassword, saltRounds);
-    user.password = newPassword;
-    await this.adminRepo.save(user);
-    return content.PasswordUpdated;
   }
 
   /**
@@ -161,39 +111,6 @@ export class AdminService {
     }
     this.adminRepo.remove(admin);
     return `${id} ${content.Deleted}`;
-  }
-
-  async isLockedOut(adminId: string): Promise<boolean> {
-    const lockoutRecord = await this.getLockoutRecord(adminId);
-    if (!lockoutRecord) {
-      return false;
-    }
-    const expired = await this.checkLockoutExpired(lockoutRecord);
-    if (expired) {
-      return false;
-    }
-    return lockoutRecord.attempts >= maximumAttempts;
-  }
-
-  async addFailedAttempt(adminId: string) {
-    let lockoutRecord = await this.getLockoutRecord(adminId);
-    if (!lockoutRecord) {
-      lockoutRecord = {
-        admin: await this.getAdmin(adminId),
-        attempts: 0,
-      } as Lockout;
-    }
-    lockoutRecord.attempts++;
-    await this.lockoutRepo.save(lockoutRecord);
-  }
-
-  private async checkLockoutExpired(lockout: Lockout): Promise<boolean> {
-    const expired = new Date(lockout.expiry) < new Date();
-    if (!expired) {
-      return false;
-    }
-    await this.lockoutRepo.remove(lockout);
-    return true;
   }
 
   /**
